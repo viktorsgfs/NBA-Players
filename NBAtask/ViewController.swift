9ae1204472c82968c975d03a3e2d95756c8d4129
Viktor <vkrastev97@yandex.ru>
2023-07-08 18:26:11 +0300

    Initial Commit

---
+++ NBAtask/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NBAtask
+//
+//  Created by Viktor Krastev on 8.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
